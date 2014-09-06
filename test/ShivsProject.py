6ae679222468f4a7145adb899407ac336b587de0
Akiira <Hudsonrj09@gmail.com>
2014-09-05 22:02:52 -0400

    Just playing with some test code

--- test/ShivsProject.py
+++ test/ShivsProject.py
@@ -13,7 +13,11 @@ import pygraph
 
 f = open("63382.txt")
 raw = f.read()
-
+sentences = nltk.sent_tokenize(raw)
+#words = []
+#print(tokens)
+#print(nltk.word_tokenize("Witnesses, which simply record the status of the file but contain no data, can be used in addition to or in place of files to reduce overhead."))
+print(nltk.pos_tag(nltk.word_tokenize("Witnesses, which simply record the status of the file but contain no data, can be used in addition to or in place of files to reduce overhead.")))
 
 # Frequency, takes a word and a document
 def f(w, d):
